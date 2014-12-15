49a9051c5e7e4db157736214f3cd78cb4a8c7ae5
Kalenai <kalenairedtail@gmail.com>
2014-12-15 16:36:36 -0600

    Setup for PostgreSQL, tweaked models, and implemented Flask-Migrate

--- app/models.py
+++ app/models.py
@@ -2,11 +2,12 @@ from app import db
 from passlib.apps import custom_app_context as pwd_context
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), unique=True)
     password_hash = db.Column(db.String(128))
     email = db.Column(db.String(100), unique=True)
-    about = db.Column(db.Text(1200))
+    about = db.Column(db.Text)
 
     def hash_password(self, password):
         self.password_hash = pwd_context.encrypt(password, scheme="sha512_crypt")
@@ -18,10 +19,11 @@ class User(db.Model):
         return '<User %r>' % self.username
 
 class Post(db.Model):
+    __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     timestamp = db.Column(db.DateTime)
-    post_body = db.Column(db.Text(4000))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    post_body = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     def __repr__(self):
         return '<Post %r>' % self.post_body
--- config.py
+++ config.py
@@ -1,3 +1,6 @@
 #Flask-WTF CSRF Protection
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'secret-key-of-secretness' #Placeholder; replace with a secure key before deploying
+
+#Database Config
+SQLALCHEMY_DATABASE_URI = 'postgresql://devuser:devpass@localhost:5432/devdb'
